b6ce0bc67a7bdc09c9baf9e818043f4457a2a659
Muthu <muthu.kumar@me.com>
2022-01-11 13:12:01 +0530

    removed unwanted files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+var app = express();
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get("/test", (req, res, next) => {
+  res.json({
+    "success": true
+  });
+});
+
+// Let's listen
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
