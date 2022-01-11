0d9ef4fb17b3fa65a147636118b197699c39dc7c
Muthu <muthu.kumar@me.com>
2022-01-11 13:15:17 +0530

    Added empty route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+app.get("/", (req, res, next) => {
+  res.send('Hello World!')
+});
+
 app.get("/test", (req, res, next) => {
   res.json({
     "success": true
